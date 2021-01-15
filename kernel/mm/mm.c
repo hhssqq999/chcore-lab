09a773082378ce17f67b833e796390fb1e6e8c78
hhssqq9999 <hhssqq9999@gmail.com>
2021-01-15 17:08:31 +0800

    finish merge lab2

--- kernel/mm/mm.c
+++ kernel/mm/mm.c
@@ -103,11 +103,5 @@ void mm_init(void)
 
 	map_kernel_space(KBASE + (128UL << 21), 128UL << 21, 128UL << 21);
 	//check whether kernel space [KABSE + 256 : KBASE + 512] is mapped 
-<<<<<<< HEAD
 	// kernel_space_check();
-=======
-	kernel_space_check();
-
-	
->>>>>>> lab2-sol
 }
